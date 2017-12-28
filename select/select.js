384fa0f976e937562e8a88fa66fe291525929db8
SweetAbby <1078120759@qq.com>
2017-12-28 23:52:36 +0800

    'data'

---
+++ select/select.js
@@ -0,0 +1,22 @@
+new Vue({
+    el:"#app",
+    data:{
+        datas:[
+            {id:1,title:"1111"},
+            {id:2,title:"2222"},
+            {id:3,title:"3333"},
+            {id:4,title:"4444"}
+        ],
+        state:false,
+        title:''
+    },
+    methods:{
+        aa(){
+            this.state=true;
+        },
+        bb(val){
+            this.state=false;
+            this.title=val;
+        }
+    }
+})
